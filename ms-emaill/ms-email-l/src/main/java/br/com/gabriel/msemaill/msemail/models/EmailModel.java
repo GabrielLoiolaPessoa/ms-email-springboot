7337a9af432e1cf7e6f3e9159e784883f8102605
Gabriel Pessoa <gabriedeloiolacontato@gmail.com>
2022-11-06 16:08:29 -0300

    First commit with the class EmailModel implemented

---
+++ ms-emaill/ms-email-l/src/main/java/br/com/gabriel/msemaill/msemail/models/EmailModel.java
@@ -0,0 +1,31 @@
+package br.com.gabriel.msemaill.msemail.models;
+
+import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDataEmail;
+    private StatusEmail statusEmail;
+
+
+}
