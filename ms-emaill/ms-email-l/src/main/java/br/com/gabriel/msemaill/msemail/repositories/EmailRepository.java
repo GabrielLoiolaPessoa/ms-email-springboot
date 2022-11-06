da2cc4464faa2ee242f537ae5c9be86558a8dce6
Gabriel Pessoa <gabriedeloiolacontato@gmail.com>
2022-11-06 17:10:27 -0300

    some changes

--- ms-emaill/ms-email-l/src/main/java/br/com/gabriel/msemaill/msemail/repositories/EmailRepository.java
+++ ms-emaill/ms-email-l/src/main/java/br/com/gabriel/msemaill/msemail/repositories/EmailRepository.java
@@ -3,9 +3,9 @@ package br.com.gabriel.msemaill.msemail.repositories;
 import br.com.gabriel.msemaill.msemail.models.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+import java.util.UUID;
 
-
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 
 
 }
--- ms-emaill/ms-email-l/src/main/java/br/com/gabriel/msemaill/msemail/services/EmailService.java
+++ ms-emaill/ms-email-l/src/main/java/br/com/gabriel/msemaill/msemail/services/EmailService.java
@@ -6,12 +6,16 @@ import br.com.gabriel.msemaill.msemail.models.EmailModel;
 import br.com.gabriel.msemaill.msemail.repositories.EmailRepository;
 import org.aspectj.bridge.IMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class EmailService {
@@ -41,5 +45,12 @@ public class EmailService {
             return emailRepository.save(emailModel);
         }
 
+    }
+    public Page<EmailModel> findAll(Pageable pageable) {
+        return  emailRepository.findAll(pageable);
+    }
+
+    public Optional<EmailModel> findById(UUID emailId) {
+        return emailRepository.findById(emailId);
     }
 }
